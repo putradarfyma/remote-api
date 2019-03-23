98bd343759819601d6159519f0a066238ad6064d
richasdy <donni.richasdy@gmail.com>
2019-03-23 16:24:33 +0700

    add pytorch dnn

---
+++ pytorch_cnn.py
@@ -0,0 +1,2 @@
+# source : https://adventuresinmachinelearning.com/convolutional-neural-networks-tutorial-in-pytorch/
+# source : https://github.com/adventuresinML/adventures-in-ml-code
---
+++ pytorch_tut.py
@@ -0,0 +1,3 @@
+# source : https://github.com/yunjey/pytorch-tutorial
+# source : https://github.com/andrewliao11/dni.pytorch
+# source : https://towardsdatascience.com/multi-layer-perceptron-usingfastai-and-pytorch-9e401dd288b8
